bf271bc118bd48b8910f0cd629dc01ccf0df0761
randahu <randa.hu01@gmail.com>
2025-10-18 15:14:11 +0200

    shared

--- projekt.Api/obj/Debug/net9.0/projekt.Api.AssemblyInfo.cs
+++ projekt.Api/obj/Debug/net9.0/projekt.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projekt.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c1fe0fc884d6cf01f9d696e5941f0be85ed3758")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+734a71360e0ef76f95ccf68cb8f64d1d936e87c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("projekt.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projekt.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
